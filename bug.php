d75a6e6dbf0667e8fab711eae5ea98474dcb5a4b
kylekatarnls <kylekatarnls@gmail.com>
2021-04-09 12:32:33 +0200

    Dump the whole object

--- bug.php
+++ bug.php
@@ -3,7 +3,8 @@
 $dtToronto = new DateTime('2021-04-09 00:00 America/Toronto');
 $dtVancouver = new DateTime('2021-04-09 00:00 America/Vancouver');
 
+$diff = $dtToronto->diff($dtVancouver);
+var_dump($diff);
 $hours = $dtToronto->diff($dtVancouver)->format('%h');
-echo $hours;
 
 exit($hours === '3' ? 0 : 1);
