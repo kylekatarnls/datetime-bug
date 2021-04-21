0e7cf853a296f7ad1b5b40cea7e079675f28634a
kylekatarnls <kylekatarnls@gmail.com>
2021-04-21 21:36:21 +0200

    Timezone and year shift

--- bug.php
+++ bug.php
@@ -1,33 +1,10 @@
 <?php
 
-$dtToronto = new DateTime('2012-01-01 00:00:00.000000 America/Toronto');
-$dtVancouver = new DateTime('2012-01-01 00:00:00.000000 America/Vancouver');
+$dtToronto = DateTime::createFromFormat('!Y-n-j G:i:s.u', '0-01-01 00:00:00.000000', new DateTimeZone('America/Toronto'));
+$dtVancouver = DateTime::createFromFormat('!Y-n-j G:i:s.u', '0-01-01 00:00:00.000000', new DateTimeZone('America/Vancouver'));
 
-$diff = $dtToronto->diff($dtVancouver);
-
-var_dump($diff);
-
-$diff = $dtVancouver->diff($dtToronto);
-
-var_dump($diff);
-
-// DateTimeZone
-
-$dtToronto = new DateTime('2012-01-01 00:00:00.000000', new DateTimeZone('America/Toronto'));
-$dtVancouver = new DateTime('2012-01-01 00:00:00.000000', new DateTimeZone('America/Vancouver'));
-
-$diff = $dtToronto->diff($dtVancouver);
-
-var_dump($diff);
-
-$diff = $dtVancouver->diff($dtToronto);
-
-var_dump($diff);
-
-// createFromFormat
-
-$dtToronto = DateTime::createFromFormat('!Y-n-j G:i:s.u', '2012-01-01 00:00:00.000000', new DateTimeZone('America/Toronto'));
-$dtVancouver = DateTime::createFromFormat('!Y-n-j G:i:s.u', '2012-01-01 00:00:00.000000', new DateTimeZone('America/Vancouver'));
+$dtToronto->modify('+2012 years');
+$dtVancouver->modify('+2012 years');
 
 $diff = $dtToronto->diff($dtVancouver);
 
