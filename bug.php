ac355d7859f7d1327110ffbc45b1b28dcc12a4ce
kylekatarnls <kylekatarnls@gmail.com>
2021-04-21 20:11:55 +0200

    Test both ways diffs

--- bug.php
+++ bug.php
@@ -1,41 +1,13 @@
 <?php
 
-$dtToronto = new DateTime('2021-04-09 00:00 America/Toronto');
-$dtVancouver = new DateTime('2021-04-09 00:00 America/Vancouver');
+$dtToronto = new DateTime('2012-01-01 00:00:00.000000 America/Toronto');
+$dtVancouver = new DateTime('2012-01-01 00:00:00.000000 America/Vancouver');
 
 $diff = $dtToronto->diff($dtVancouver);
 
-function getDateIntervalSpec(DateInterval $interval): string
-{
-    $date = array_filter([
-        'Y' => abs($interval->y),
-        'M' => abs($interval->m),
-        'D' => abs($interval->d),
-    ]);
+var_dump($diff);
 
-    $time = array_filter([
-        'H' => abs($interval->h),
-        'M' => abs($interval->i),
-        'S' => abs($interval->s),
-    ]);
-
-    $specString = 'P';
-
-    foreach ($date as $key => $value) {
-        $specString .= $value.$key;
-    }
-
-    if (\count($time) > 0) {
-        $specString .= 'T';
-        foreach ($time as $key => $value) {
-            $specString .= $value.$key;
-        }
-    }
-
-    return $specString === 'P' ? 'PT0S' : $specString;
-}
-
-$diff = new DateInterval(getDateIntervalSpec($diff));
+$diff = $dtVancouver->diff($dtToronto);
 
 var_dump($diff);
 
