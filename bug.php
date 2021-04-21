ea07c1444c5ada001bf8194a9dcc4e9fed224248
kylekatarnls <kylekatarnls@gmail.com>
2021-04-21 21:11:17 +0200

    Construct with timezones

--- bug.php
+++ bug.php
@@ -11,6 +11,17 @@ $diff = $dtVancouver->diff($dtToronto);
 
 var_dump($diff);
 
+$dtToronto = new DateTime('2012-01-01 00:00:00.000000', new DateTimeZone('America/Toronto'));
+$dtVancouver = new DateTime('2012-01-01 00:00:00.000000', new DateTimeZone('America/Vancouver'));
+
+$diff = $dtToronto->diff($dtVancouver);
+
+var_dump($diff);
+
+$diff = $dtVancouver->diff($dtToronto);
+
+var_dump($diff);
+
 $hours = $diff->format('%h');
 
 exit($hours === '3' ? 0 : 1);
