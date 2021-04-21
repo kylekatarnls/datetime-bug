baadbd9ebbc93e0aa71e2956e8336475e1d9ec78
kylekatarnls <kylekatarnls@gmail.com>
2021-04-21 21:14:43 +0200

    Construct with createFromFormat

--- bug.php
+++ bug.php
@@ -11,8 +11,23 @@ $diff = $dtVancouver->diff($dtToronto);
 
 var_dump($diff);
 
+// DateTimeZone
+
 $dtToronto = new DateTime('2012-01-01 00:00:00.000000', new DateTimeZone('America/Toronto'));
 $dtVancouver = new DateTime('2012-01-01 00:00:00.000000', new DateTimeZone('America/Vancouver'));
+
+$diff = $dtToronto->diff($dtVancouver);
+
+var_dump($diff);
+
+$diff = $dtVancouver->diff($dtToronto);
+
+var_dump($diff);
+
+// createFromFormat
+
+$dtToronto = DateTime::createFromFormat('!Y-n-j G:i:s.u', '2012-01-01 00:00:00.000000', new DateTimeZone('America/Toronto'));
+$dtVancouver = DateTime::createFromFormat('!Y-n-j G:i:s.u', '2012-01-01 00:00:00.000000', new DateTimeZone('America/Vancouver'));
 
 $diff = $dtToronto->diff($dtVancouver);
 
