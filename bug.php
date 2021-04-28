debf5b040ac7a3b3ab9fd91a150aee751a170f5e
kylekatarnls <kylekatarnls@gmail.com>
2021-04-28 11:02:55 +0200

    Check inverted interval

--- bug.php
+++ bug.php
@@ -1,12 +1,12 @@
 <?php
 
-$dtToronto = new DateTime('2012-01-01 00:00:00.000000 America/Toronto');
-$dtVancouver = new DateTime('2012-01-01 00:00:00.000000 America/Vancouver');
+$date = new DateTime('2021-04-05 14:00:00');
+$interval = new DateInterval('P0DT10799S');
+$interval->f = 0.999999;
+$interval->invert = 1;
+$date->add($interval);
+$string = $date->format('Y-m-d H:i:s.u');
 
-$diff = $dtVancouver->diff($dtToronto);
+echo $string;
 
-var_dump($diff);
-
-$hours = $diff->format('%h');
-
-exit($hours === '3' ? 0 : 1);
+exit($string === '2021-04-05 11:00:00.000001' ? 0 : 1);
