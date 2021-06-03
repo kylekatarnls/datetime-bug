3f52779f5c248be67bda6f3e9e572ec42b82d6cf
kylekatarnls <kylekatarnls@gmail.com>
2021-06-03 10:03:38 +0200

    Compare DST 1 day shift handling

--- bug.php
+++ bug.php
@@ -1,12 +1,22 @@
 <?php
 
-$date = new DateTime('2021-04-05 14:00:00');
-$interval = new DateInterval('P0DT10799S');
-$interval->f = 0.999999;
-$interval->invert = 1;
-$date->add($interval);
-$string = $date->format('Y-m-d H:i:s.u');
+echo (new DateTimeImmutable('2010-11-07T00:00:00 America/New_York'))->diff(
+    new DateTimeImmutable('2010-11-08T00:00:00 America/New_York')
+)->format('%d %h') . "\n";
+echo (new DateTimeImmutable('2010-03-14T00:00:00 America/New_York'))->diff(
+    new DateTimeImmutable('2010-03-15T00:00:00 America/New_York')
+)->format('%d %h') . "\n";
+echo "\n\n";
 
-echo $string;
+echo (new DateTimeImmutable('2010-11-07T00:00:00 America/New_York'))->modify('24 hours')->format('j G') . "\n";
+echo (new DateTimeImmutable('2010-11-07T00:00:00 America/New_York'))->add(new DateInterval('PT24H'))->format('j G') . "\n";
+echo "\n";
+echo (new DateTimeImmutable('2010-11-07T00:00:00 America/New_York'))->modify('1 day')->format('j G') . "\n";
+echo (new DateTimeImmutable('2010-11-07T00:00:00 America/New_York'))->add(new DateInterval('P1D'))->format('j G') . "\n";
+echo "\n\n";
+echo (new DateTimeImmutable('2010-03-14T00:00:00 America/New_York'))->modify('24 hours')->format('j G') . "\n";
+echo (new DateTimeImmutable('2010-03-14T00:00:00 America/New_York'))->add(new DateInterval('PT24H'))->format('j G') . "\n";
+echo "\n";
+echo (new DateTimeImmutable('2010-03-14T00:00:00 America/New_York'))->modify('1 day')->format('j G') . "\n";
+echo (new DateTimeImmutable('2010-03-14T00:00:00 America/New_York'))->add(new DateInterval('P1D'))->format('j G') . "\n";
 
-exit($string === '2021-04-05 11:00:00.000001' ? 0 : 1);
