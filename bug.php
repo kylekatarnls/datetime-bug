0dd916c7d220e02fbe7ac15c8ce9f7310f897a84
kylekatarnls <kylekatarnls@gmail.com>
2021-06-05 00:46:50 +0200

    Try half max

--- bug.php
+++ bug.php
@@ -1,6 +1,6 @@
 <?php
 
-$greater = (new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate(PHP_INT_MAX, 12, 31) >
+$greater = (new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate(4611686018427387904, 12, 31) >
     new DateTimeImmutable('2019-01-26 10:30:12.000000');
 
 var_dump($greater);
