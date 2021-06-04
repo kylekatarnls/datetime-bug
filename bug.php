b147f52ae84dba63441e37dc0bd689e4fd7537f9
kylekatarnls <kylekatarnls@gmail.com>
2021-06-05 00:54:49 +0200

    Bisect minimum

--- bug.php
+++ bug.php
@@ -1,12 +1,12 @@
 <?php
 
-$min = 2001;
-$max = 4611686018427387904;
+$min = -91732385841753851055;
+$max = 1999;
 
 while ($max - $min > 1) {
     $half = (int) (($max + $min) / 2);
 
-    if ((new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate($half, 12, 31) >
+    if ((new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate($half, 12, 31) <
         new DateTimeImmutable('2019-01-26 10:30:12.000000')) {
         $min = $half;
 
@@ -17,10 +17,10 @@ while ($max - $min > 1) {
 }
 
 var_dump($min, $max);
-$okMin = (new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate($min, 12, 31) >
+$okMin = (new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate($min, 12, 31) <
     new DateTimeImmutable('2019-01-26 10:30:12.000000');
 var_dump($okMin,
-    (new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate($max, 12, 31) >
+    (new DateTimeImmutable('2000-12-31 23:59:59.999999'))->setDate($max, 12, 31) <
     new DateTimeImmutable('2019-01-26 10:30:12.000000'));
 
 exit($okMin ? 0 : 1);
